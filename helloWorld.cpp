58b1373a4b78839a528ed8e54b5f059e2f3ca6ba
Yiyun Qin <yiyun.qin@mths.ca>
2022-02-18 18:17:55 +0000

    helloWorld program

---
+++ helloWorld.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2022 Yiyun Qin All rights reserved
+//
+// Created by Yiyun Qin
+// Created at February 2022
+// This is a "Hello World" program with proper style
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+}
